5a931b804946414ee8f9ed017f693a6b83378e7e
Pagwin <git@pagwin.xyz>
2023-07-26 15:28:42 -0400

    a few more configuration things

--- init.lua
+++ init.lua
@@ -7,6 +7,11 @@ vim.opt.rnu = true
 vim.opt.wrap = true
 vim.opt.shiftwidth = 4
 vim.opt.mouse = ""
+--- backup file prefs
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 ---- plugin management
 require("lazy").setup(require("plugins"))
--- lua/hotkeys.lua
+++ lua/hotkeys.lua
@@ -5,10 +5,15 @@ vim.g.mapleader = ' '
 -- to make Y consistent with D and P
 vim.api.nvim_set_keymap("n","Y","y$",{})
 
---- convenience hotkeys for diagnostics
+--- convenience hotkeys for diagnostics and generally figuring out what's gone wrong
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 
 --- undotree
 vim.keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+
+--- clipboard convenience
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
