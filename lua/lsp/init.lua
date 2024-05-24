742fb18ac29eacba715c903da2f74539498da67c
Pagwin <git@pagwin.xyz>
2024-05-24 19:56:36 -0400

    some lsps

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -22,9 +22,11 @@ lspconfig.jedi_language_server.setup{}
 lspconfig.julials.setup{}
 lspconfig.kotlin_language_server.setup{}
 lspconfig.lua_ls.setup{}
+lspconfig.ocamllsp.setup{}
 lspconfig.omnisharp.setup{}
 lspconfig.phpactor.setup{}
 lspconfig.pyright.setup{}
+lspconfig.r_language_server.setup{}
 lspconfig.rust_analyzer.setup{}
 lspconfig.texlab.setup{}
 lspconfig.tsserver.setup{}
